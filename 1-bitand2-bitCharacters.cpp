062a86c753e1c2c7a593dc431252e688999abb93
Aatman Vaidya <aatmanvaidya@gmail.com>
2021-07-20 22:13:20 +0530

    first commit

---
+++ 1-bitand2-bitCharacters.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int i = 0;
+
+        while (i < bits.size()-1)
+            if (bits[i])
+                i += 2;
+            else
+                ++i;
+
+        return i == bits.size()-1;
+    }
+};
