6c0990e8d395e83b0cd4b6dd30d4d8b203e55a1a
Aatman Vaidya <aatmanvaidya@gmail.com>
2021-10-02 22:39:10 +0530

    2 Oct 2021

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int rev = 0;
+        while (rev < x) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return x == rev || x == rev / 10;
+    }
+};
