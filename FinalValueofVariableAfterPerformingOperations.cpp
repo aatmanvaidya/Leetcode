0a76326aaa748aed6895e0297e641b8cc2c2d63b
Aatman Vaidya <aatmanvaidya@gmail.com>
2023-01-02 22:51:52 +0530

    2/1/23

---
+++ FinalValueofVariableAfterPerformingOperations.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int n = operations.size() , X=0;
+        for(int i=0;i<n;i++){
+            if(operations[i]=="--X" || operations[i]=="X--" )  X--;
+            else X++;
+        }
+        return X;
+    }
+};
