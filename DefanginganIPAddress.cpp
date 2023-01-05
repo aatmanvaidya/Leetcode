52141431a92246fb50b23c0776fca5c3066140f3
aatmanvaidya <aatmanvaidya@gmail.com>
2023-01-05 17:47:39 +0530

    5/1/23

---
+++ DefanginganIPAddress.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans ="";
+        for(char x:address)
+        {
+            if(x =='.')
+               ans = ans + "[.]";
+            else
+               ans = ans + x; 
+        }
+        return ans;
+    }
+};
